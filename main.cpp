191b5b0f886f54e71b5f3bf3a7f3da0b270630b7
Mohamadreza Irick <mohamadresaaa@gmail.com>
2026-05-09 00:18:26 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,3 +11,5 @@ int main(int argc, char *argv[]) {
 	
 	return 0;
 }
+
+vless://bc9389e8-25d8-4bcb-9924-17fd2aac12b7@s.ar-panelar.com:8443?alpn=h2%2Chttp%2F1.1&encryption=none&fp=chrome&host=&mode=auto&path=%2F&security=tls&sni=s.ar-panelar.com&type=xhttp#huvxqi2c-187.54MB%F0%9F%93%8A
